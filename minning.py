2d0c4a68523e305b2e2ce1fb757919f835f3e7b2
amakhyon <a.m.makhyon@gmail.com>
2021-07-22 10:55:32 +0200

    control number of letters per word
    now you can control how many zeros do you want
    and how many words per letter
    useful for finding corelation

--- minning.py
+++ minning.py
@@ -3,9 +3,9 @@ import hashlib
 
 
 
-def get_word():
+def get_word(letters):
 	word = []
-	for i in range(8):
+	for i in range(letters):
 		letter = random.randint(33,1415)
 		word.append(chr(letter))
 	alpha_word = ""	
@@ -20,15 +20,16 @@ def insha(string):
 	return result.hexdigest()
 
 
-def get_hash_prefixed_zeros(zeros):
+def get_hash_prefixed_zeros(zeros,letters):
 	while True:	
 		prefix = '0' * zeros
-		word = get_word()
+		word = get_word(letters)
 		wordy = insha(word)	
 		while not wordy.startswith(prefix):
-			word = get_word()
+			word = get_word(letters)
 			wordy = insha(word)		
 		print(word)
 
 prefix = int(input("how many zeros do you need? : "))
-get_hash_prefixed_zeros(prefix)
+letters = int(input("how many letters per word? : "))
+get_hash_prefixed_zeros(prefix, letters)
